bb86b098138fdec83db079419a2a14b74f9319ca
mu <goodness897@gmail.com>
2016-01-30 16:26:51 +0900

    git first

---
+++ app/src/main/java/com/example/mu/myapplication/ItemView.java
@@ -0,0 +1,41 @@
+package com.example.mu.myapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ItemView extends FrameLayout {
+    public ItemView(Context context){
+        super(context);
+        init();
+    }
+
+    ImageView iconView;
+    TextView titleView, typeView, contentView, menu1View, menu2View;
+
+
+    private void init() {
+        LayoutInflater.from(getContext()).inflate(R.layout.item_layout,this);
+        iconView = (ImageView)findViewById(R.id.image_icon);
+        titleView = (TextView)findViewById(R.id.text_title);
+        typeView = (TextView)findViewById(R.id.text_type);
+        contentView = (TextView)findViewById(R.id.text_content);
+        menu1View = (TextView)findViewById(R.id.text_menu1);
+        menu2View = (TextView)findViewById(R.id.text_menu2);
+    }
+    ItemData mData;
+    public void setItemData(ItemData data){
+        mData = data;
+        iconView.setImageResource(data.imageId);
+        titleView.setText(data.title);
+        typeView.setText(data.type);
+        contentView.setText(data.content);
+        menu1View.setText(data.menu1);
+        menu2View.setText(data.menu2);
+
+    }
+
+}
+
